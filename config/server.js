27f81b29032c1379724316781284e2486bb029fc
Xun <8533432+lixun2333@user.noreply.gitee.com>
2021-07-21 03:28:17 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6e104268a314cab0e7d392dcdbf23af1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d25fe08-c76c-49be-83de-4abd148fba3f'
+};
