fcf987cb78466b20b55e80e13f1e906512dca1e1
Xun <8533432+lixun2333@user.noreply.gitee.com>
2021-07-21 09:53:55 +0800

    fix:数据库信息更改

--- config/database.js
+++ config/database.js
@@ -1,15 +1,38 @@
+/*
+ * @Author: XunL
+ * @Date: 2021-07-18 20:57:04
+ * @LastEditTime: 2021-07-21 09:53:10
+ * @Description: file content
+ */
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'gridsome_blog'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'meng6666'),
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
